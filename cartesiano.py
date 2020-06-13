6349e58fe205af823c495805d3d7d8b5968436be
PauloGunther <pauloaugustogunther@gmail.com>
2020-06-13 17:22:19 -0300

    Mais

---
+++ cartesiano.py
@@ -0,0 +1,11 @@
+def cartesiano():
+    x = {'n': 1, 's': -1}
+    y = {'e': 1, 'w': -1}
+    lx = l.remove()
+
+
+le = ['n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's', 'e']
+# cartesiano(l)
+for i in le:
+    if i in 'e':
+        
